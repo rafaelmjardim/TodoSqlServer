ef55a0a8d3ef2ebf8c0addbda32525870f2d1e78
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-07-21 20:25:06 -0300

    feat: Adicionado valor default nas classes

--- TodoSqlServer/Models/User.cs
+++ TodoSqlServer/Models/User.cs
@@ -3,11 +3,11 @@
     public class User
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
 
         //Relacionamento: 1 usuário pode ter varias tarefas
-        public ICollection<TodoItem> TodoItems { get; set; }
+        public ICollection<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
     }
 }
